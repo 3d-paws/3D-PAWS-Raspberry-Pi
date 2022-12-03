1558b975763d071338ffb9ba27a30166b97a90da
Joey <jrener@ucar.edu>
2022-12-02 20:31:06 -0700

    Null values are no longer reported to chords

--- scripts/upkeep/report.py
+++ scripts/upkeep/report.py
@@ -42,6 +42,7 @@ def checkFile(sensor):
 if now.minute % record_interval == 0 and test_toggle == "false":
     #data = [bmp_temp, bmp_pressure, bmp_slp, bmp_altitude, bme_temp, bme_pressure, bme_slp, bme_altitude, bme_humidity, htu21d_temp, htu21d_humidity, mcp9808, rain, si1145_vis, si1145_ir, si1145_uv, wind_direction, wind_speed]
     data = [-999.99, -999.99, -999.99, -999.99, -999.99, -999.99, -999.99, -999.99, -999.99, -999.99, -999.99, -999.99, -999.99, -999.99, -999.99, -999.99, -999.99, -999.99]
+    url = "http://%s/measurements/url_create?instrument_id=%d" % (chords_link, chords_id)
 
     bmp = checkFile("bmp")
     if bmp:
@@ -49,6 +50,9 @@ if now.minute % record_interval == 0 and test_toggle == "false":
         data[1] = float(bmp[6])
         data[2] = float(bmp[7])
         data[3] = float(bmp[8])
+        if data[0] != -999.99:  
+            url += "&bmp_temp=%05.1f&bmp_pressure=%07.2f&bmp_slp=%07.2f&bmp_altitude=%07.2f" % (data[0], data[1], data[2], data[3])
+            
     bme = checkFile("bme")
     if bme:
         data[4] = float(bme[5])
@@ -56,27 +60,47 @@ if now.minute % record_interval == 0 and test_toggle == "false":
         data[6] = float(bme[7])
         data[7] = float(bme[8])
         data[8] = float(bme[9])
+        if data[4] != -999.99:  
+            url += "&bme_temp=%05.1f&bme_pressure=%07.2f&bme_slp=%07.2f&bme_altitude=%07.2f&bme_humidity=%07.2f" % (data[4], data[5], data[6], data[7], data[8])
+            
     htu21d = checkFile("htu21d")
     if htu21d:
         data[9] = float(htu21d[5])
         data[10] = float(htu21d[6])
+        if data[9] != -999.99:  
+            url += "&htu21d_temp=%05.1f&htu21d_humidity=%04.1f" %(data[9], data[10])
+            
     mcp9808 = checkFile("mcp9808")
     if mcp9808:
         data[11] = float(mcp9808[5])
+        if data[11] != -999.99:  
+            url += "&mcp9808=%05.1f" % (data[11])
+
     rain = checkFile("rain")
     if rain:
         data[12] = float(rain[5])
+        if data[12] != -999.99:  
+            url += "&rain=%04.2f" % (data[12])
+            
     si1145 = checkFile("si1145")
     if si1145:
         data[13] = float(si1145[5])
         data[14] = float(si1145[6])
         data[15] = float(si1145[7])
+        if data[13] != -999.99:  
+            url += "&si1145_vis=%010.1f&si1145_ir=%010.1f&si1145_uv=%010.1f" % (data[13], data[14], data[15])
+            
     wind_direction = checkFile("wind_direction")
     if wind_direction:
         data[16] = float(wind_direction[6])
+        if data[16] != -999.99:  
+            url += "&wind_direction=%05.1f" % (data[16])
+
     wind_speed = checkFile("wind_speed")
     if wind_speed:
         data[17] = float(wind_speed[5])
+        if data[17] != -999.99:  
+            url += "&wind_speed=%04.2f" % (data[17])
 
 
     #save to daily file if data is being recorded
@@ -85,8 +109,9 @@ if now.minute % record_interval == 0 and test_toggle == "false":
 
 
     #report to chords if it's time to
-    url = "http://%s/measurements/url_create?instrument_id=%d&bmp_temp=%05.1f&bmp_pressure=%07.2f&bmp_slp=%07.2f&bmp_altitude=%07.2f&bme_temp=%05.1f&bme_pressure=%07.2f&bme_slp=%07.2f&bme_altitude=%07.2f&bme_humidity=%07.2f&htu21d_temp=%05.1f&htu21d_humidity=%04.1f&mcp9808=%05.1f&rain=%04.2f&si1145_vis=%010.1f&si1145_ir=%010.1f&si1145_uv=%010.1f&wind_direction=%05.1f&wind_speed=%04.2f&key=21DE6A8A" % (chords_link, chords_id, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15], data[16], data[17])	
+    #url = "http://%s/measurements/url_create?instrument_id=%d&bmp_temp=%05.1f&bmp_pressure=%07.2f&bmp_slp=%07.2f&bmp_altitude=%07.2f&bme_temp=%05.1f&bme_pressure=%07.2f&bme_slp=%07.2f&bme_altitude=%07.2f&bme_humidity=%07.2f&htu21d_temp=%05.1f&htu21d_humidity=%04.1f&mcp9808=%05.1f&rain=%04.2f&si1145_vis=%010.1f&si1145_ir=%010.1f&si1145_uv=%010.1f&wind_direction=%05.1f&wind_speed=%04.2f&key=21DE6A8A" % (chords_link, chords_id, data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15], data[16], data[17])	
     if now.minute % chords_interval == 0 and chords_toggle == "true": 
+        url += "&key=21DE6A8A"
         try:
             requests.get(url=url)
             #output(True, url, "/home/pi/3d_paws/logs/chords_testing.log")
