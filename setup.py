84c83f1d4986abf37cae87894935d346330c7ee3
3d-paws <jrener@ucar.edu>
2024-08-05 10:23:39 -0600

    Added a new dependancy

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
             "adafruit_circuitpython_HTU21D==0.10.2",
             "adafruit_circuitpython_sht31d==2.3.0",
             "adafruit_circuitpython_mcp9808==3.3.2",
+            "adafruit_circuitpython_busdevice==5.0.5",
             "adafruit_mcp9808==1.5.6",
             "gps==3.19",
             "python_crontab==2.6.0",
